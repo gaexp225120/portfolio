d76da2fb4e8a4f462bb3c29ac2975ec00455d716
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2024-07-25 15:47:36 +0800

    update  main layout
    update main layout

--- app/page.tsx
+++ app/page.tsx
@@ -15,51 +15,52 @@ export default function Home() {
     toolkit: null,
     project: null,
   });
-
   return (
-    <div className="min-h-screen bg-custom-gradient px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-      <div className="lg:flex flex-col lg:flex-row lg:justify-between lg:gap-4 max-w-screen-xl mx-auto">
-        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-          <SideNavBar sideBarRefs={sideBarRefs} />
-        </header>
-        <main className="lg:w-1/2 lg:py-24">
-          {/* <ProgressIndicator /> */}
-          <section
-            ref={(el: HTMLDivElement | null) => {
-              sideBarRefs.current.about = el;
-            }}
-            id="about"
-            className="scroll-mt-16 lg:scroll-mt-24"
-          >
-            <Introduction />
-          </section>
-          <section
-            ref={(el: HTMLDivElement | null) => {
-              sideBarRefs.current.experience = el;
-            }}
-            id="experience"
-            className="scroll-mt-16 lg:scroll-mt-18"
-          >
-            <Experience />
-          </section>
-          <section
-            ref={(el: HTMLDivElement | null) => {
-              sideBarRefs.current.toolkit = el;
-            }}
-            id="toolkit"
-            className="negative-scroll-mt-4"
-          >
-            <TechStack />
-          </section>
-        </main>
+    <div>
+      <div className="bg-custom-gradient px-3 py-12 font-sans sm:px-6 md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:gap-4 max-w-screen-xl mx-auto">
+          <header className="lg:sticky lg:top-0 lg:w-1/2 lg:flex lg:flex-col lg:justify-between lg:py-24 w-full">
+            <SideNavBar sideBarRefs={sideBarRefs} />
+          </header>
+          <main className="lg:w-1/2 lg:py-24 w-full">
+            {/* <ProgressIndicator /> */}
+            <section
+              ref={(el: HTMLDivElement | null) => {
+                sideBarRefs.current.about = el;
+              }}
+              id="about"
+              className="scroll-mt-16 lg:scroll-mt-24"
+            >
+              <Introduction />
+            </section>
+            <section
+              ref={(el: HTMLDivElement | null) => {
+                sideBarRefs.current.experience = el;
+              }}
+              id="experience"
+              className="scroll-mt-16 lg:scroll-mt-18"
+            >
+              <Experience />
+            </section>
+            <section
+              ref={(el: HTMLDivElement | null) => {
+                sideBarRefs.current.toolkit = el;
+              }}
+              id="toolkit"
+              className="scroll-mt-16 lg:scroll-mt-18"
+            >
+              <TechStack />
+            </section>
+          </main>
+        </div>
       </div>
-      <footer className="py-12 w-full">
+      <footer className="p-3 sm:p-12 w-full">
         <section
           ref={(el: HTMLDivElement | null) => {
             sideBarRefs.current.skill = el;
           }}
           id="project"
-          className=" mx-auto"
+          className="mx-auto"
         >
           <Contact />
         </section>
